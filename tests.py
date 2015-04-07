434d4ea1e02cbdefd348d52dd9f7e83f91ed2574
Ben Youngblut <by@fotosearch.com>
2015-04-07 14:49:59 -0500

    switched to nosetests

--- tests.py
+++ tests.py
@@ -1,51 +1,37 @@
-import sys
+import subprocess
 
-from unittest import TestCase, TestSuite, TextTestRunner
-from collections import OrderedDict
-from subprocess import Popen, PIPE
-
-## STEM TESTS
-def stem_tests(*args):
-    def decorator(method, args=args):
-        for arg in args:
-            def method_for_arg(self, method=method, arg=arg):
-                method(self, *arg)
-            name = method.__name__ + '(' + '->'.join(arg[:2]) + ')'
-            frame = sys._getframe(1)  # pylint: disable-msg=W0212
-            frame.f_locals[name] = method_for_arg
-        return None
-    return decorator
-
-## STEMMING TEST CASE
-class StemmingTestCase(TestCase):
-    ## RUN TEST
-    def stem(self, input):
-        process = Popen(
-            args = ['hunspell', '-m', '-d', 'en_US', '-s'],
-            stdin = PIPE,
-            stdout = PIPE,
-        )
-        (stdout, stderr) = process.communicate(input)
-        return stdout.replace(input, '').strip()
-
-    ## TEST STEMMING
-    @stem_tests(
-        ('men', 'man', True),
-        ('women', 'woman', True),
-        ('conmen', 'conman', True),
-        ('congressmen', 'congressman', True),
-        ('people', 'person', True),
-        ('teeth', 'tooth', True),
-        ('mothers', 'mother', True),
-        ('telemarketer', 'telemarket', True),
-        ('telemarketers', 'telemarket', True),
-        ('telemarketing', 'telemarket', True),
-        ('butter', 'butt', False),
-        ('corner', 'corn', False),
-        ('easter', 'east', False),
+## STEMMING TESTS
+STEMS = [
+    ('men', 'man', True),
+    ('women', 'woman', True),
+    ('conmen', 'conman', True),
+    ('congressmen', 'congressman', True),
+    ('people', 'person', True),
+    ('teeth', 'tooth', True),
+    ('mothers', 'mother', True),
+    ('telemarketer', 'telemarket', True),
+    ('telemarketers', 'telemarket', True),
+    ('telemarketing', 'telemarket', True),
+    ('butter', 'butt', False),
+    ('corner', 'corn', False),
+    ('easter', 'east', False),
+]
+## TEST STEMMING
+def test_stemming():
+    for (input, output, equal) in STEMS:
+        yield check_stem, input, output, equal
+## CHECK STEM
+def check_stem(input, output, equal):
+    if equal:
+        assert stem(input) == output, 'SHOULD STEM %s -> %s'%(input, output)
+    else:
+        assert stem(input) <> output,'SHOULD NOT STEM %s -> %s'%(input, output)
+## STEM
+def stem(input):
+    process = subprocess.Popen(
+        args = ['hunspell', '-m', '-d', 'en_US', '-s'],
+        stdin = subprocess.PIPE,
+        stdout = subprocess.PIPE,
     )
-    def test_stemming(self, input, output, equal):
-        if equal:
-            self.assertEqual(self.stem(input), output, 'SHOULD STEM %s -> %s'%(input, output))
-        else:
-            self.assertNotEqual(self.stem(input), output, 'SHOULD NOT STEM %s -> %s'%(input, output))
+    (stdout, stderr) = process.communicate(input)
+    return stdout.replace(input, '').strip()
