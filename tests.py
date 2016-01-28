861331ec2bb817c03283db9e510100676d65502f
brkastner <bk.horizon@gmail.com>
2016-01-28 14:05:27 -0600

    make the tester object oriented and asynchronous

--- tests.py
+++ tests.py
@@ -1,27 +1,41 @@
 from subprocess import Popen, PIPE
 
-def stem(input):
-    process = Popen(
-        args = ['hunspell', '-m', '-d', 'en_US', '-s'],
-        stdin = PIPE,
-        stdout = PIPE,
-    )
-    (stdout, stderr) = process.communicate(input)
-    return stdout.replace(input, '').strip()
+import asyncio
 
-def test_stemming():
-    with open('tests.txt') as file:
-        for line in file:
-            if line.startswith('#'):
-                continue
-            line = line.rstrip('\n').split('#')[0].strip()
-            try:
-                (input, output) = line.split(';')
-                yield validate, input.strip(), output.strip()
-            except ValueError:
-                if line:
-                    yield validate, line, line
 
-def validate(input, output):
-    stemmed = stem(input)
-    assert stemmed == output or (stemmed == '' and input == output), 'input: {}, expected: {}, actual: {}'.format(input, output, stemmed)
+class StemmingTester:
+    @staticmethod
+    def stem(inp):
+        process = Popen(
+            args=['hunspell', '-m', '-d', 'en_US', '-s'],
+            stdin=PIPE,
+            stdout=PIPE,
+        )
+        stdout, stderr = process.communicate(inp)
+        return stdout.replace(inp, ''.encode('UTF-8')).strip()
+
+    async def validate(self, input, output):
+        stemmed = self.stem(input)
+        if not (stemmed == output or (stemmed == '' and input == output)):
+            print('input: {}, expected: {}, actual: {}'.format(input, output, stemmed))
+
+    async def test(self):
+        with open('tests.txt') as file:
+            for line in file:
+                if line.startswith('#'):
+                    continue
+                line = line.rstrip('\n').split('#')[0].strip()
+                try:
+                    inp, output = line.split(';')
+                    self.validate(inp.strip(), output.strip())
+                except ValueError:
+                    if line:
+                        self.validate(line, line)
+
+
+if __name__ == '__main__':
+    tester = StemmingTester()
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(tester.test())
+    loop.close()
